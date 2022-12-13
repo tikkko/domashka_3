6ad66a17e8643a940a0859f9132b0959391206e7
tikkko <tix.yefimenko@gmail.com>
2022-12-13 22:05:05 +0200

    commit1

--- domashka3.py
+++ domashka3.py
@@ -8,30 +8,67 @@ import random
 # def get_random_values(choices, size=2):
 #    return random.choices(choices, k=size)
 
-def get_random_value():
-    return random.choice((1, 2, 3, 4, 5))
+# def get_random_value():
+#    return random.choice((1, 2, 3, 4, 5))
+def repeat(n): #Допоміжна функція для розуміння логіки
+    for i in range(n):
+        print(2 * n)
 
 
-def retry(attempts=5, desired_value=None):
+def decorate(i): #Допоміжна функція для розуміння логіки
     def wrapper(func):
-        def inner_wrapper(*args, **kwargs):
-
-            for arg in args:
-                if attempts > 5:
-                    print('attempts limit reached')
-
-            for arg in kwargs:
-                if desired_value is not None:
-                    print('true')
+        def inner_wrapper(*args):
+            for arg in range(i):
+                print(func(arg) + arg)
 
         return inner_wrapper
 
     return wrapper
 
 
-@retry(desired_value=4)
+@decorate(5) #Допоміжна функція для розуміння логіки
+def get_one(n):
+    return n * 2
+
+
+def retry(attempts=5, desired_value=None):
+    def wrapper(func):
+        def inner_wrapper(*args, **kwargs):
+            out_of_attempts = True
+            for arg in range(attempts):
+                if len(args) > 0:
+                    a = func(args[0])
+                else:
+                    a = func()
+                if a == desired_value:
+                    out_of_attempts = False
+                    break
+
+            if out_of_attempts:
+                print('out of attempts')
+            else:
+                print(desired_value)
+            return
+
+
+        return inner_wrapper
+
+    return wrapper
+
+
+@retry(attempts=5, desired_value=3)
 def get_random_value():
     return random.choice((1, 2, 3, 4, 5))
 
-# if __name__ == '__main__':
-# get_random_value()
+@retry(attempts=5, desired_value=[1, 2, 3])
+def get_random_values(choices, size=3):
+    return random.choices(choices, k=size)
+
+
+
+if __name__ == '__main__':
+    print(get_random_value())
+   # print(get_random_values((1, 2, 3, 1, 2)))
+   # print(get_one(3))
+   # print(repeat(5))
+
--- main.py
+++ main.py
@@ -1,10 +1,38 @@
 
-def print_square(n):
-     if n == int:
-         print:'*' * n
-    elif
+def print_square_wrong(n):  #Допоміжна функція для розуміння логіки
+    if n == 1:
+        print('*')
+    else:
+        print('*' * n)
+        for i in range(n - 2):
+            print('*' + (' ' * (n-2)) + '*')
+        print('*' * n)
+
+
+
+
+def print_square(n, i = 0):
+    if n < 1 or n > 220:
+        print('Please write the correct parameter - n. In range [1 - 220]')
+        return
+    if i == 0:
+        i = n
+        if i == 1:
+            print('*')
+            return
+        n = (n - 2)
+        print('*' * i)
+    if n <= 1:
+        if n == 1:
+            print('*' + (' ' * (i - 2)) + '*')
+        print('*' * i)
+    else:
+        print('*' + (' ' * (i-2)) + '*')
+        print_square(n - 1, i)
 
 
 if __name__ == '__main__':
-    print(print_square(5))
+    #print(print_square(4))
+    #print(raw_square(4, 1))
+    print_square(221)
 
